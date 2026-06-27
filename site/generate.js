5d0733a24003cf8bcc1b3a8256d5870175e2c93a
TanakaTe24 <293481865+TanakaTe24@users.noreply.github.com>
2026-06-27 11:07:28 +0900

    modify generate.js

--- site/generate.js
+++ site/generate.js
@@ -8,6 +8,19 @@ lines.shift(); // ヘッダー削除
 
 let rows = "";
 
+rows += "<h1>S1リーグ</h1>";
+rows += `
+    <table>
+        <thead>
+            <tr>
+                <th>選手1</th>
+                <th>選手2</th>
+                <th>結果</th>
+            </tr>
+        </thead>
+        <tbody>
+`;
+
 for (const line of lines) {
     const [p1, p2, result] = line.split(",");
 
@@ -18,6 +31,12 @@ for (const line of lines) {
     <td>${result}</td>
 </tr>`;
 }
+
+rows += `
+        </tbody>
+    </table>
+`;
+
 
 let html = fs.readFileSync("template/index.html", "utf8");
 
